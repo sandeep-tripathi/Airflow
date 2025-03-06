2877155fd99c06594ab093ec3f6f0f6278d11f67
Sandeep Tripathi <39844255+sandeep-tripathi@users.noreply.github.com>
2025-03-06 22:30:18 +0100

    Update email_report_task.py

--- email_report_task.py
+++ email_report_task.py
@@ -1,23 +1,40 @@
-from airflow.operators.email import EmailOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.sensors.filesystem import FileSensor
+from datetime import datetime
 
-# Assuming you've already defined your DAG named report_dag and the generate_report task
-report_dag = DAG('report_workflow', start_date=datetime(2024, 1, 20), schedule_interval='@monthly')
+# Define default args
+default_args = {
+    'email': ['airflow@datacamp.com'],
+    'email_on_failure': True,
+    'email_on_success': True,
+}
 
-generate_report = BashOperator(
+# Define DAG
+report_dag = DAG(
+    dag_id='execute_report',
+    schedule_interval="0 0 * * *",
+    default_args=default_args,
+    start_date=datetime(2023, 2, 20),
+    catchup=False  # Avoid backfilling for dates before the start_date
+)
+
+# Define tasks
+precheck = FileSensor(
+    task_id='check_for_datafile',
+    filepath='salesdata_ready.csv',
+    start_date=datetime(2023, 2, 20),
+    mode='reschedule',
+    dag=report_dag
+)
+
+generate_report_task = BashOperator(
     task_id='generate_report',
     bash_command='generate_report.sh',
+    start_date=datetime(2023, 2, 20),
     dag=report_dag
 )
 
-# Define the email task
-email_report = EmailOperator(
-    task_id='email_report',
-    to='airflow@datacamp.com',
-    subject='Airflow Monthly Report',
-    html_content="""Attached is your monthly workflow report - please refer to it for more detail""",
-    files=['/path/to/monthly_report.pdf'],
-    dag=report_dag
-)
+# Set task dependencies
+precheck >> generate_report_task
 
-# Set the email task to run after the report is generated
-generate_report >> email_report
